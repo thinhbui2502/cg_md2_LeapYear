2ecefe601dfbc4b068bb0e8b35c3316929fb8199
mrty2502 <61007059+mrty2502@users.noreply.github.com>
2020-03-19 19:05:06 +0700

    add file LeapYear

---
+++ src/LeapYear.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a year!");
+        int year = scanner.nextInt();
+        boolean isLeapYear = false;
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if (isDivisibleBy4) {
+            boolean isDivisibleBy100 = year % 100 == 0;
+            if (isDivisibleBy100) {
+                boolean isDivisibleBy400 = year % 400 == 0;
+                if (isDivisibleBy400) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+        if (isLeapYear) {
+            System.out.printf("%d is leap year", year);
+        } else {
+            System.out.printf("%d is not leap year", year);
+        }
+    }
+}
